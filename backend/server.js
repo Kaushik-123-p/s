a3c68ac75d0943a7947044f1d849b46c67a7492d
Prajapati Kaushik <kaushik@example.com>
2025-04-23 18:40:35 +0530

    add product model and API endpoint for product creation

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,32 @@
 import express from "express"
 import dotenv from "dotenv"
 import { connectDB } from "./db/db.js"
+import { Product } from "./models/product.models.js"
+
 
 dotenv.config()
 
 const app = express()
 
-app.get("/", (req, res) => {
-  res.send("Server is ready")
+app.use(express.json())
+
+app.post("/api/v1/products", async (req, res) => {
+  const product = req.body
+
+  if (!product.name || !product.price || !product.image) {
+    return res.status(400).json({ success: false, message: "Please Provide all fields!" })
+  }
+
+  const newProduct = new Product(product)
+
+  try {
+    await newProduct.save()
+    res.status(201).json({ success: true, data: newProduct })
+  } catch (error) {
+    console.error("Error create in product : ", error.message)
+    res.status(500).json({ success: false, message: "Server FAILED!" })
+  }
+
 })
 
 app.listen(5000, () => {
