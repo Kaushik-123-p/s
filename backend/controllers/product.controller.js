6c8e54ac73d3de7a0e8dec5937b9327387ff5662
Prajapati Kaushik <kaushik@example.com>
2025-04-24 10:51:27 +0530

    implement product controller with CRUD operations and update routes import path

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,4 +1,4 @@
-import { Product } from "../../models/product.models.js"
+import { Product } from "../models/product.models.js"
 import mongoose from "mongoose"
 
 
--- backend/routers/products.routes.js
+++ backend/routers/products.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getProducts, createProduct, updateProduct, deleteProduct } from "./controllers/product.controller.js";
+import { getProducts, createProduct, updateProduct, deleteProduct } from "../controllers/product.controller.js"
 
 const router = Router()
 
