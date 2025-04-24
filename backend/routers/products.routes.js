aab236260fd97fbee8e796f827828c720e679e12
Prajapati Kaushik <kaushik@example.com>
2025-04-24 10:07:43 +0530

    create routers folder in create product.routes.js file and  copy routers with function it pest in routes .

---
+++ backend/routers/products.routes.js
@@ -0,0 +1,73 @@
+import { Router } from "express";
+import { Product } from "./models/product.models.js"
+import mongoose from "mongoose"
+
+const router = Router()
+
+
+router.get("/", async (req, res) => {
+  try {
+    const products = await Product.find({})
+    res.status(200).json({ success: true, data: products })
+  } catch (error) {
+    console.log("error in feaching products!", error.message)
+    res.status(500).json({ success: false, message: "Server Error!" })
+  }
+})
+
+// ---------------------------------------------------------------------
+router.post("/", async (req, res) => {
+  const product = req.body
+
+  if (!product.name || !product.price || !product.image) {
+    return res.status(400).json({ success: false, message: "Please Provide all fields!" })
+  }
+
+  const newProduct = new Product(product)
+
+  try {
+    await newProduct.save()
+    res.status(201).json({ success: true, data: newProduct })
+  } catch (error) {
+    console.error("Error create in product : ", error.message)
+    res.status(500).json({ success: false, message: "Server FAILED!" })
+  }
+
+})
+
+// -----------------------------------------------------------------------
+router.put("/:id", async (req, res) => {
+  const { id } = req.params
+
+  const products = req.body
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ success: false, message: "invalid product id" })
+  }
+
+
+  try {
+    const updetedProduct = await Product.findByIdAndUpdate(id, products, { new: true })
+    res.status(200).json({ success: true, data: updetedProduct })
+  } catch (error) {
+    console.log("error in updation products", error.message)
+    res.status(500).json({ success: true, message: "server error" })
+  }
+})
+
+// -----------------------------------------------------------------------
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params
+  console.log("id", id)
+
+  try {
+    await Product.findByIdAndDelete(id)
+    res.status(200).json({ success: true, message: "Product deleted successfuly." })
+  } catch (error) {
+    console.log("Error  indeleting produc :", error.message)
+    res.status(404).json({ success: false, message: "product not found!" })
+  }
+})
+
+
+export default router
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import { connectDB } from "./db/db.js"
-import { Product } from "./models/product.models.js"
-import mongoose from "mongoose"
+import productRouters from "./routers/products.routes.js"
 
 
 dotenv.config()
@@ -11,69 +10,7 @@ const app = express()
 
 app.use(express.json())
 
-app.get("/api/v1/getproducts", async (req, res) => {
-  try {
-    const products = await Product.find({})
-    res.status(200).json({ success: true, data: products })
-  } catch (error) {
-    console.log("error in feaching products!", error.message)
-    res.status(500).json({ success: false, message: "Server Error!" })
-  }
-})
-
-// ---------------------------------------------------------------------
-app.post("/api/v1/products", async (req, res) => {
-  const product = req.body
-
-  if (!product.name || !product.price || !product.image) {
-    return res.status(400).json({ success: false, message: "Please Provide all fields!" })
-  }
-
-  const newProduct = new Product(product)
-
-  try {
-    await newProduct.save()
-    res.status(201).json({ success: true, data: newProduct })
-  } catch (error) {
-    console.error("Error create in product : ", error.message)
-    res.status(500).json({ success: false, message: "Server FAILED!" })
-  }
-
-})
-
-// -----------------------------------------------------------------------
-app.put("/api/v1/products/:id", async (req, res) => {
-  const { id } = req.params
-
-  const products = req.body
-
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).json({ success: false, message: "invalid product id" })
-  }
-
-
-  try {
-    const updetedProduct = await Product.findByIdAndUpdate(id, products, { new: true })
-    res.status(200).json({ success: true, data: updetedProduct })
-  } catch (error) {
-    console.log("error in updation products", error.message)
-    res.status(500).json({ success: true, message: "server error" })
-  }
-})
-
-// -----------------------------------------------------------------------
-app.delete("/api/v1/products/:id", async (req, res) => {
-  const { id } = req.params
-  console.log("id", id)
-
-  try {
-    await Product.findByIdAndDelete(id)
-    res.status(200).json({ success: true, message: "Product deleted successfuly." })
-  } catch (error) {
-    console.log("Error  indeleting produc :", error.message)
-    res.status(404).json({ success: false, message: "product not found!" })
-  }
-})
+app.use("/api/v1/products", productRouters)
 
 app.listen(5000, () => {
   connectDB()
